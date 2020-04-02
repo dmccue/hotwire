87080c8e84ae9991cfdc8c9552b30e17406f3d2e
Ubuntu <ubuntu@ip-172-26-9-197.eu-west-2.compute.internal>
2020-04-02 00:06:32 +0000

    updates to wg key creation

--- installHost.sh
+++ installHost.sh
@@ -2,12 +2,13 @@
 
 lsb_release -r | grep 18.04 || exit 1
 
+
 # Setup SSH
 sudo sed -i 's/^PasswordAuthentication/#PasswordAuthentication/' /etc/ssh/sshd_config
 sudo service sshd restart
-sudo apt update
 
-# Disable systemd resolvconf
+
+# Disable systemd resolver proxy 
 sudo systemctl disable systemd-resolved.service
 sudo service systemd-resolved stop
 sudo rm /etc/resolv.conf
@@ -17,42 +18,41 @@ search eu-west-2.compute.internal" | sudo tee /etc/resolv.conf >/dev/null
 
 
 # Install Docker
-sudo apt remove -y docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker ubuntu
+which docker || {
+  sudo apt remove -y docker docker-engine docker.io containerd runc
+  sudo apt update
+  sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt update
+  sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo groupadd docker
+  sudo usermod -aG docker ubuntu
+}
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+which docker-compose || {
+  curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose 2>/dev/null && sudo mv /tmp/docker-compose /usr/local/bin/
+  sudo chmod +x /usr/local/bin/docker-compose
+}
 
 # Install hotwire repo
-su ubuntu -c -l 'cd $HOME; git clone https://github.com/dmccue/hotwire.git'
-su ubuntu -c -l 'cd $HOME/hotwire; docker-compose up'
-
-curl ifconfig.me | sudo tee /root/external_ip
-
-
-# Setup Docker
-#sudo mkdir /data
-#sudo modprobe ip6table_filter
-#sudo docker rm -f subspace
-#sudo docker run -d --name subspace --restart always --network host --cap-add NET_ADMIN --volume /usr/bin/wg:/usr/bin/wg --volume /data:/data --env SUBSPACE_HTTP_HOST=$(hostname -f) subspacecloud/subspace:latest
-
+ls $HOME/hotwire >/dev/null || {
+  su ubuntu -cl 'cd $HOME && git clone https://github.com/dmccue/hotwire.git && cd $HOME/hotwire && docker-compose up'
+}
 
 
 # Install wireguard
-sudo add-apt-repository ppa:wireguard/wireguard
-sudo apt-get update
-sudo apt-get install -y wireguard
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sudo mkdir /etc/wireguard
-sudo apt install -y qrencode
+which wg || {
+  echo "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list.d/backports.list
+  sudo add-apt-repository -y ppa:wireguard/wireguard
+  sudo apt update
+  sudo apt install -y wireguard resolvconf qrencode
+  sudo sysctl -w net.ipv4.ip_forward=1
+  sudo sysctl -w net.ipv6.conf.all.forwarding=1
+  sudo mkdir /etc/wireguard
+  sudo apt install -y qrencode
+}
 
 echo "
 [Interface]
@@ -68,4 +68,23 @@ PublicKey = ndayypaI3yKssziQuOMFoDb/IAyD8JRYWZ00wKUdlz8=
 PresharedKey = tUJyt5HSlg+n+VLrSZfAEBxcnpu7KT8Z1zBGXpd2E6M=
 AllowedIPs = 10.19.49.2/32
 " | sudo tee /etc/wireguard/wg0.conf
+
+# Finalise
+#wg-quick up /etc/wireguard/wg0.conf
+git clone https://github.com/burghardt/easy-wg-quick.git $HOME/easy-wg-quick
+mkdir /data/wireguard
+curl ifconfig.me/ip > extnetip.txt
+echo 51820 > portno.txt
+$HOME/easy-wg-quick/easy-wg-quick $(date +%s)
+sudo wg-quick down /data/wireguard/wghub.conf
+sudo wg-quick up /data/wireguard/wghub.conf
+curl ifconfig.me 2>/dev/null | sudo tee /root/external_ip > /dev/null
+sudo cat /etc/wireguard/wg0.conf | qrencode -t ansiutf8 | tee /root/wireguard.qr
+
+
+# Setup Subspace
+#sudo mkdir /data
+#sudo modprobe ip6table_filter
+#sudo docker rm -f subspace
+#sudo docker run -d --name subspace --restart always --network host --cap-add NET_ADMIN --volume /usr/bin/wg:/usr/bin/wg --volume /data:/data --env SUBSPACE_HTTP_HOST=$(hostname -f) subspacecloud/subspace:latest
 
