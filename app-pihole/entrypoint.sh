ac09f74d9800fc669d2b07114039a886ceab2dcf
dmccue <dmccue@gmail.com>
2020-05-07 01:55:08 +0100

    dns bugfixes

--- app-pihole/entrypoint.sh
+++ app-pihole/entrypoint.sh
@@ -7,4 +7,6 @@ NET_LOCALDOMAINRESOLVERIP="127.0.0.11"
 echo "server=/$NET_LOCALREVDNSDOMAIN/$NET_LOCALDOMAINRESOLVERIP" > /etc/dnsmasq.d/05-custom.conf
 echo "server=/$NET_LOCALDNSDOMAIN/$NET_LOCALDOMAINRESOLVERIP" >> /etc/dnsmasq.d/05-custom.conf
 
+
 /s6-init
+pihole restartdns
